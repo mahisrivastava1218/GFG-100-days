933975445451ad3d6db8141fcdf2fc356d8a11fb
mahisrivastava1218 <98544175+mahisrivastava1218@users.noreply.github.com>
2022-09-28 23:53:40 +0530

    Create DAY7-array fitting.java

---
+++ DAY7-array fitting.java
@@ -0,0 +1,20 @@
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static boolean isFit (int arr[], int brr[], int n) {
+        //Complete the function
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>brr[i]){
+                    return false;
+            }
+        }
+    
+    return true;
+    
+    }
+    
+}
